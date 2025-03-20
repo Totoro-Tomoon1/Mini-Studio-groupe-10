ff39387214fccfc6ca596cc392e58f14601be251
Calbrieux <calbrieux@gaming.tech>
2025-03-20 16:46:34 +0100

    continue chest

--- src/LightEngine/Chest.h
+++ src/LightEngine/Chest.h
@@ -7,6 +7,14 @@ class Chest : public Entity
 {
 	Animation* mAnimation;
 	sf::Texture mTexture;
+
+	bool mHaveKey = false;
+
+public:
+	void OnOpen();
+
+protected:
+	void OnCollision(Entity* collidedWith) override;
 		
 };
 
